6e5601b91ee7908a299072abffc298154b5bb835
Sean Huber <sean@onmyblock.com>
2014-12-09 13:30:45 -0800

    Add a health#show endpoint to make sure an application is up
    Includes rspec tests

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,5 @@
+class HealthController < ApplicationController
+  def show
+    render text: 'OK'
+  end
+end
---
+++ spec/controllers/health_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe HealthController do
+  context 'GET show' do
+    before { get :show }
+
+    it { should respond_with :success }
+
+    it 'should render OK' do
+      expect(response.body).to eq 'OK'
+    end
+  end
+end
