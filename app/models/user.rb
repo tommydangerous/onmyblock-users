b98b0dca1800cb2c24e9fce45ed07b4262c3a8b6
Tommy Dang <quantumventuress@gmail.com>
2014-12-10 14:58:19 -0800

    added sessions controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,17 @@
 class User < BaseModel
-  include ActiveModel::Model
+  # ROLES = {
+  #   administrator: "administrator"
+  #   landlord:      "landlord",
+  #   student:       "student"
+  # }.freeze
   
-  field :email, type: String
+  field :email,      type: String
   field :first_name, type: String
-  field :last_name, type: String
+  field :last_name,  type: String
+  field :role,       type: Array
 
-  validates_format_of :email, 
+  validates_format_of :email,
     with: /\A[\w]([^@\s,;]+)@(([\w-]+\.)+([A-Za-z0-9]*))\z/i
   validates_presence_of :email, :first_name, :last_name
+  # validates_inclusion_of :role, in: ROLES, if: role_exists?
 end
