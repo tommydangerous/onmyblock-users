13d9e8298aa130e8b76625e3a6785483ba97c9a9
Sean Huber <sean@onmyblock.com>
2014-12-10 14:49:23 -0800

    Replace single quotes with double quotes

--- app/controllers/health_controller.rb
+++ app/controllers/health_controller.rb
@@ -1,5 +1,5 @@
 class HealthController < ApplicationController
   def show
-    render text: 'OK'
+    render text: "OK"
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,11 +1,11 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV['RAILS_ENV'] = 'test'
-require 'spec_helper'
+# This file is copied to spec/ when you run "rails generate rspec:install"
+ENV["RAILS_ENV"] = "test"
+require "spec_helper"
 require File.expand_path("../../config/environment", __FILE__)
-require 'rspec/rails'
+require "rspec/rails"
 
 # Add additional requires below this line. Rails is not loaded until this point!
-require 'payload/testing'
+require "payload/testing"
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -29,10 +29,10 @@ require 'payload/testing'
 RSpec.configure do |config|
   config.include Payload::Testing
 
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # Remove this line if you"re not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # If you"re not using ActiveRecord, or you"d prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
