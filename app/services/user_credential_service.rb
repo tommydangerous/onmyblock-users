1bc4dc0ef79f6dae4acae3ee4a4ff457b1819be3
Tommy Dang <quantumventuress@gmail.com>
2014-12-11 16:37:07 -0800

    added test for record_valid

--- app/services/user_credential_service.rb
+++ app/services/user_credential_service.rb
@@ -5,19 +5,15 @@ class UserCredentialService < CrudService
   end
 
   def build_credential
-    create_credential_service(credential_params).record
+    create_credential_service.record
   end
 
   def build_user
-    create_user_service(options).record
+    create_user_service.record
   end
 
-  def create_credential_service(opts)
-    create_service Credential, opts
-  end
-
-  def create_user_service(opts)
-    create_service User, opts
+  def record_valid?(record)
+    record.valid?
   end
 
   def process
@@ -31,8 +27,16 @@ class UserCredentialService < CrudService
 
   private
 
+  def create_credential_service
+    @create_credential_service ||= create_service Credential, credential_params
+  end
+
   def create_service(model, opts)
     CreateService.new model, opts
+  end
+
+  def create_user_service
+    @create_user_service ||= create_service User, options
   end
 
   def credential_params
--- spec/services/user_credential_service_spec.rb
+++ spec/services/user_credential_service_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe UserCredentialService do
+  let(:build_credential) { service.build_credential }
+  let(:build_user) { service.build_user }
   let(:service) { UserCredentialService.new service_options }
   let(:service_options) { 
     FactoryGirl.attributes_for(:user).merge({ password: "password" })
@@ -8,35 +10,32 @@ RSpec.describe UserCredentialService do
 
   describe "#build_credential" do
     it "should return a built credential with the same attributes" do
-      credential = service.build_credential
-      expect(credential.identification).to eq(
+      expect(build_credential.identification).to eq(
         service_options[:email])
-      expect(credential.password).to eq service_options[:password]
+      expect(build_credential.password).to eq service_options[:password]
     end
   end
 
   describe "#build_user" do
     it "should return a built user with the same attributes" do
-      user = service.build_user
-      expect(user.email).to eq service_options[:email]
-      expect(user.first_name).to eq service_options[:first_name]
-      expect(user.last_name).to eq service_options[:last_name]
+      expect(build_user.email).to eq service_options[:email]
+      expect(build_user.first_name).to eq service_options[:first_name]
+      expect(build_user.last_name).to eq service_options[:last_name]
     end
   end
 
-  context "services" do
-    let(:service) { UserCredentialService.new({}, nil) }
-
-    describe "#create_credential_service" do
-      it "should return a CreateService object with credential record" do
-        expect(service.create_credential_service({}).record.class
-          ).to eq Credential
+  describe "#record_valid?" do
+    context "with valid attributes" do
+      it "should return true" do
+        expect(service.record_valid?(build_user)).to be true
       end
     end
 
-    describe "#create_user_service" do
-      it "should return a CreateService object with user record" do
-        expect(service.create_user_service({}).record.class).to eq User
+    context "with invalid attributes" do
+      let(:service_options) { {} }
+
+      it "should return false" do
+        expect(service.record_valid?(build_user)).to be false
       end
     end
   end
