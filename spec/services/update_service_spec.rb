e0d5e519c4dc53bd5169516eaeb3270bb3d92b77
Tommy Dang <quantumventuress@gmail.com>
2014-12-15 10:23:20 -0800

    added record_id to update service

--- spec/services/update_service_spec.rb
+++ spec/services/update_service_spec.rb
@@ -2,14 +2,15 @@ require_relative "../../app/services/update_service"
 require_relative "../../spec/support/collection_mock"
 
 RSpec.describe UpdateService do
-  subject { described_class.new model, options }
+  subject { described_class.new model, record_id, options }
 
   let(:email)     { "test@gmail.com" }
   let(:id)        { rand(100) }
   let(:model)     { CollectionMock }
   let(:new_email) { "new_test@gmail.com" }
-  let(:options)   { { id: id, email: new_email } }
+  let(:options)   { { email: new_email } }
   let(:record)    { model.create valid_attributes }
+  let(:record_id) { record.id }
   let(:valid_attributes) { { id: id, email: email } }
 
   describe "#process" do
@@ -27,7 +28,8 @@ RSpec.describe UpdateService do
     end
 
     context "when record not found" do
-      let(:options) { { id: "", email: new_email } }
+      let(:options) { { email: new_email } }
+      let(:record_id) { "" }
 
       it "should not update record" do
         expect(record.email).to eq email
