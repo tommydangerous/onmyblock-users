0dfe0ebcc5d08eacaf3f554162d5262cf337b791
Sean Huber <sean@onmyblock.com>
2014-12-16 16:15:57 -0800

    Make sure the `key` test factory has a unique token

--- spec/support/factories.rb
+++ spec/support/factories.rb
@@ -15,7 +15,7 @@ FactoryGirl.define do
   factory :key do
     credential
     expires_at Time.zone.now + 7.days
-    token SecureRandom.uuid
+    token { |n| "token-#{n}" }
   end
 
   factory :user do
