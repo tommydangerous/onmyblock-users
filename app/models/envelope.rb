cbeccbf45176675d42561ca6363cc24a3fce57fc
Tommy Dang <quantumventuress@gmail.com>
2014-12-17 22:08:14 -0800

    updated envelope

--- app/models/envelope.rb
+++ app/models/envelope.rb
@@ -4,13 +4,13 @@ class Envelope
   attr_reader :errors, :metadata, :resource
 
   def initialize(opts = {})
-    @errors   = opts[:errors]   || {}
+    @errors   = opts[:errors] ? opts[:errors] : {}
     @metadata = add_metadata opts
-    @resource = opts[:resource] || {}
+    @resource = opts[:resource] ? opts[:resource] : {}
   end
 
   def add_metadata(opts)
-    hash = opts[:metadata] || {}
+    hash = opts[:metadata] ? opts[:metadata] : {}
     hash[:execution_time] = Time.now
     hash[:status] = opts[:status] if opts[:status]
     hash
