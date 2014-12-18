b82fd70355f1655c4cc38660a9e2d309212e528d
Tommy Dang <quantumventuress@gmail.com>
2014-12-17 21:45:59 -0800

    added request helpers from users app

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,7 +20,7 @@ require "payload/testing"
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -31,6 +31,12 @@ RSpec.configure do |config|
   config.include Mongoid::Matchers, type: :model
   config.include Payload::Testing
   config.include Shoulda::Matchers
+
+  config.include Request::EnvelopeHelpers, type: :controller
+  config.include Request::HeadersHelpers, type: :controller
+  # config.before :each, type: :controller do
+  #   include_default_accept_headers
+  # end
 
   config.before :suite do
     FactoryGirl.lint
