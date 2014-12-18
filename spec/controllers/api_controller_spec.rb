17d9f8e890c75e6a4a6f405b06296c2c78ae5d7a
Tommy Dang <quantumventuress@gmail.com>
2014-12-17 17:09:09 -0800

    updated api tests

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -3,11 +3,60 @@ require "rails_helper"
 RSpec.describe ApiController do
   let(:controller) { described_class.new }
 
+  describe "#delete_service" do
+    it "should receive :service message" do
+      expect(controller).to receive :service
+      controller.send :delete_service, Class, {}
+    end
+  end
+
+  describe "#package_envelope" do
+    let(:condition) { true }
+    let(:hash)      { controller.send :package_envelope, service, 200, 404 }
+    let(:service)   { double "service", process: condition, response: {} }
+
+    context "when service process is true" do
+      it "should return a hash with key errors" do
+        expect(hash).to have_key :errors
+      end
+
+      it "should return a hash with key resource" do
+        expect(hash).to have_key :resource
+      end
+
+      it "should return a hash with key status" do
+        expect(hash).to have_key :status
+      end
+    end
+
+    context "when service process is false" do
+      let(:condition) { false }
+
+      it "should return a hash with key errors" do
+        expect(hash).to have_key :errors
+      end
+
+      it "should return a hash with key resource" do
+        expect(hash).to have_key :resource
+      end
+
+      it "should return a hash with key status" do
+        expect(hash).to have_key :status
+      end
+    end
+  end
+
   describe "#render_envelope" do
     it "should create a new Envelope" do
       allow(controller).to receive(:render)
       expect(Envelope).to receive :new
       controller.send :render_envelope, {}
+    end
+  end
+
+  describe "#service" do
+    it "should set an instance variable" do
+      expect(controller.send :service, "create", Class, {}, nil).not_to be_nil
     end
   end
 end
