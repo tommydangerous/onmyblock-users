ac3a6966a39a30d9e9b3bcf8d14cbb27861bbe7d
Tommy Dang <quantumventuress@gmail.com>
2014-12-18 15:21:31 -0800

    finished creating mail envelope

--- app/serializers/mail_envelope_serializer.rb
+++ app/serializers/mail_envelope_serializer.rb
@@ -1,1 +1,7 @@
+class MailEnvelopeSerializer < ActiveModel::Serializer
+  attributes :action, :mailer, :payload
 
+  def payload
+    object.instance_values.symbolize_keys.except(:action).except(:mailer)
+  end
+end
--- app/serializers/mail_envelope_serializer_spec.rb
+++
@@ -1,7 +0,0 @@
-require "rails_helper"
-
-RSpec.describe MailEnvelopeSerializer do
-  let(:envelope) { MailEnvelope.new options }
-  let(:hash)     { described_class.new(envelope).serializable_hash }
-  let(:options)  { {} }
-end
