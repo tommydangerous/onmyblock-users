bac1710cde6e1a04740eed2551a9b302bf83aa11
Tommy Dang <quantumventuress@gmail.com>
2014-12-17 16:45:01 -0800

    added render_envelope

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,10 @@
 class ApiController < ApplicationController
   protect_from_forgery with: :null_session
   respond_to :json
+
+  private
+
+  def render_envelope(opts)
+    render json: Envelope.new(opts)
+  end
 end
