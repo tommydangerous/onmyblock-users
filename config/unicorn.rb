73b9dbf54c9e4cf90c642749d64d9660fe13fdc9
Sean Huber <sean@onmyblock.com>
2014-12-19 10:55:43 -0800

    Autocorrect some style guid violations

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -20,20 +20,20 @@ timeout 30
 
 preload_app true
 
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   Signal.trap "TERM" do
     puts "Unicorn master intercepting TERM and sending myself QUIT instead"
     Process.kill "Quit", Process.pid
   end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   Signal.trap "TERM" do
     puts "Unicorn worker intercepting TERM and doing nothing. " \
       "Wait for master to send QUIT"
   end
 
-  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
