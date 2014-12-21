ad08ff5a5ec4f4097322e73c3ec4e584ce44f0ff
Tommy Dang <quantumventuress@gmail.com>
2014-12-21 14:19:25 -0800

    updated users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,10 +9,6 @@ class Api::V1::UsersController < Api::V1::BaseController
     render_envelope package_envelope(create_service, 200, 422)
   end
 
-  # def update
-  #   render_envelope package_envelope(update_service, 200, 422)
-  # end
-
   def update
     render_resource :update_attributes, resource.attributes
   end
@@ -26,14 +22,4 @@ class Api::V1::UsersController < Api::V1::BaseController
   def create_service
     @create_service ||= UserCredentialService.new create_params, KeySerializer
   end
-
-  # def update_params
-  #   params.require(:user).permit(:email, :first_name, :last_name, :roles)
-  # end
-
-  # def update_service
-  #   @update_service ||= UpdateService.new(
-  #     User, params[:id], update_params, UserSerializer
-  #   )
-  # end
 end
