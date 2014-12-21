997b5a734d052e52302d8d53f53826620a6f16d9
Tommy Dang <quantumventuress@gmail.com>
2014-12-21 14:43:56 -0800

    removed comments in authentications controller

--- app/controllers/api/v1/authentications_controller.rb
+++ app/controllers/api/v1/authentications_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::AuthenticationsController < Api::V1::BaseController
   include Resourceful
 
-  before_action :authenticate, only: :logout
+  before_action :authenticate,         only: :logout
   before_action :set_key_id_parameter, only: :logout
-  before_action :find_record, only: :logout
+  before_action :find_record,          only: :logout
 
   def login
     render_envelope package_envelope(login_service, 200, 401)
@@ -11,7 +11,6 @@ class Api::V1::AuthenticationsController < Api::V1::BaseController
 
   def logout
     render_resource :destroy
-    # render_envelope package_envelope(logout_service, 204, 404)
   end
 
   private
@@ -19,10 +18,6 @@ class Api::V1::AuthenticationsController < Api::V1::BaseController
   def login_service
     @login_service ||= LoginService.new params, KeySerializer
   end
-
-  # def logout_service
-  #   service "delete", Key, { id: current_session.key.id }, nil
-  # end
 
   def set_key_id_parameter
     params[:id] = current_session.key.id
