921536eb18215278b6541f51f24f77dbeed9c7ae
Sean Huber <sean@onmyblock.com>
2014-12-22 16:08:49 -0800

    Add specs for the Resourceful module

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,3 +1,7 @@
 service :metrics_client do |_container|
   Segment::Analytics.new write_key: ENV["SEGMENT_IO_KEY"]
 end
+
+factory :resource do |container|
+  Resource.new container[:controller]
+end
