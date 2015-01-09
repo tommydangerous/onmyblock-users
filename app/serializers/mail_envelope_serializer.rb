4e3cef0300286a3b6959984fd669ac7b4398d8b5
Tommy Dang <quantumventuress@gmail.com>
2015-01-09 10:41:16 -0800

    fixed mail envelope to use deliver action as opposed to just action

--- app/serializers/mail_envelope_serializer.rb
+++ app/serializers/mail_envelope_serializer.rb
@@ -3,15 +3,15 @@ class MailEnvelopeSerializer < ActiveModel::Serializer
 
   def mailer
     {
-      action:  object.action,
-      name:    object.name,
-      payload: payload
+      deliver_action:  object.deliver_action,
+      name:            object.name,
+      payload:         payload
     }
   end
 
   private
 
   def payload
-    object.instance_values.symbolize_keys.except(:action).except(:name)
+    object.instance_values.symbolize_keys.except(:deliver_action).except(:name)
   end
 end
